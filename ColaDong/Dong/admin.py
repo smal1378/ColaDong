e5dddcbd4744e2e89addfd0e16a2dc682bb3f93b
smal1378 <smal137800@gmail.com>
2026-09-18 02:12:33 +0330

    Register Payment and GroupPurchase in Django admin

--- ColaDong/Dong/admin.py
+++ ColaDong/Dong/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GroupPurchase, Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("date", "sender", "receiver", "amount", "group_purchase")
+    list_filter = ("date",)
+    search_fields = ("note",)
+    autocomplete_fields = ("sender", "receiver")
+
+
+@admin.register(GroupPurchase)
+class GroupPurchaseAdmin(admin.ModelAdmin):
+    list_display = ("date", "payer", "amount", "note", "created_at")
+    list_filter = ("date",)
+    search_fields = ("note",)
+    autocomplete_fields = ("payer",)
